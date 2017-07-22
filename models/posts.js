7a4495491ef31c086b6aadd8f87a7f98c339f94a
Chris Barnes <cjbarnes312@gmail.com>
2017-07-22 11:26:38 -0400

    updated files

--- models/posts.js
+++ models/posts.js
@@ -1,29 +1,15 @@
-
-var Sequelize = require("sequelize");
-
-var sequelize = require("../config/connection.js");
-
-module.exports = function(sequelize, dataTypes) {
-
-var post = sequelize.define("posts",  {
-  id: {
-    type: dataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
-  },
-  username: {
-    type: dataTypes.STRING,
-    allowNull: false
-    validate: { len: [0,15] }
-  },
-  user_post: {
-    type: dataTypes.STRING,
-    allowNull:false
-    validate: { len: [0,255] }
-  }
-}, {
-  timestamps: true,
-  createdAt: "Date Listed"
-});
-return post;
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Posts = sequelize.define('Posts', {
+    username: DataTypes.STRING,
+    user_posts: DataTypes.STRING
+  }, {
+    underscored: true,
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Posts;
 };
