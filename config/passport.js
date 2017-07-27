7be72e51694cc01d40f94cddf946e1d269961492
Chris Barnes <cjbarnes312@gmail.com>
2017-07-27 14:29:56 -0400

    passport with view examples

--- config/passport.js
+++ config/passport.js
@@ -1,36 +1,136 @@
-var passport = require('passport');
-var User = require('..models/user');
-var LocalStrategy = require('passport-local').Strategy;
+  //load bcrypt
+  var bCrypt = require('bcrypt-nodejs');
 
-passport.serializeuser(function(user, done){
-	done(null, user.id);
-});
+  module.exports = function(passport,user){
 
-passport.deserialzeUser(function(id,done){
-	User.findById(id,function(err,user){
-		done(err, user);
-	});
-});
+  var User = user;
+  var LocalStrategy = require('passport-local').Strategy;
 
-passport.use('local.signup', new LocalStrategy({
-	usernameField: 'email',
-	passwordField: 'password',
-	passReqtoCallback: true
-}, function(req, email,password,done){
-	user.findOne({'email':email}, function(err, user){
-		if (err){
-			return done(err);
-		}
-		if(user){
-			return done(null, false,{message: 'Email is already in use!'})
-		}
-		var newUser = new User();
-		newUser.email = email;
-		newuser.password = newUser.encryptPassword(password);
-		if(err){
-			return done(err);
-		}
-		return done(null, newUser);
-	});
 
-}));
+  passport.serializeUser(function(user, done) {
+          done(null, user.id);
+      });
+
+
+  // used to deserialize the user
+  passport.deserializeUser(function(id, done) {
+      User.findById(id).then(function(user) {
+        if(user){
+          done(null, user.get());
+        }
+        else{
+          done(user.errors,null);
+        }
+      });
+
+  });
+
+
+  passport.use('local-signup', new LocalStrategy(
+
+    {           
+      usernameField : 'email',
+      passwordField : 'password',
+      // allows to pass back the entire request to the callback
+      passReqToCallback : true
+    },
+
+    function(req, email, password, done){
+       
+
+      var generateHash = function(password) {
+      return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
+      };
+
+       User.findOne({where: {email:email}}).then(function(user){
+
+      if(user)
+      {
+        return done(null, false, {message : 'That email is already taken'} );
+      }
+
+      else
+      {
+        var userPassword = generateHash(password);
+        var data =
+        { email:email,
+        password:userPassword,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname
+        };
+
+
+        User.create(data).then(function(newUser,created){
+          if(!newUser){
+            return done(null,false);
+          }
+
+          if(newUser){
+            return done(null,newUser);
+            
+          }
+
+
+        });
+      }
+
+
+    }); 
+
+
+
+  }
+
+
+
+  ));
+    
+  //LOCAL SIGNIN
+  passport.use('local-signin', new LocalStrategy(
+    
+  {
+
+  // by default, local strategy uses username and password, we will override with email
+  usernameField : 'email',
+  passwordField : 'password',
+  // allows to pass back the entire request to the callback
+  passReqToCallback : true 
+  },
+
+  function(req, email, password, done) {
+
+    var User = user;
+
+    var isValidPassword = function(userpass,password){
+      return bCrypt.compareSync(password, userpass);
+    }
+
+    User.findOne({ where : { email: email}}).then(function (user) {
+
+      if (!user) {
+        return done(null, false, { message: 'Email does not exist' });
+      }
+
+      if (!isValidPassword(user.password,password)) {
+
+        return done(null, false, { message: 'Incorrect password.' });
+
+      }
+
+      var userinfo = user.get();
+
+      return done(null,userinfo);
+
+    }).catch(function(err){
+
+      console.log("Error:",err);
+
+      return done(null, false, { message: 'Something went wrong with your Signin' });
+
+
+    });
+
+  }
+  ));
+
+  }
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+var passport   = require('passport')
+var session    = require('express-session')
+var env = require('dotenv').load();
+var exphbs = require('express-handlebars')
+
 
 var port = process.env.PORT || 3000;
 
@@ -30,3 +35,52 @@ db.sequelize.sync({ force: true}).then(function){
 		console.log("App listening on PORT: " + PORT);
 	});
 };
+
+//testing passport
+app.get('/', function(req, res) {
+ 
+    res.send('passport is working');
+ 
+});
+ 
+ 
+app.listen(3000, function(err) {
+ 
+    if (!err)
+        console.log("Site is live");
+    else console.log(err)
+ 
+});
+//syncing/ importing models
+var models = require("./models");
+ 
+//Sync Database
+models.sequelize.sync().then(function() {
+ 
+    console.log('database working')
+ 
+}).catch(function(err) {
+ 
+    console.log(err, "problem with database")
+ 
+});
+
+// passport/express session middleware
+ 
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+ 
+app.use(passport.initialize());
+ 
+app.use(passport.session()); // persistent login sessions
+//passport views
+app.set('views', './passport/views')
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+//Routes
+var authRoute = require('./passport/routes/authroutes.js')(app,passport);
+
+//load passport strategies
+require('./config/passport.js')(passport, models.user);
